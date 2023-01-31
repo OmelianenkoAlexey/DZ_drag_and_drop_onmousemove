e405996df298e4e15436e1020a9dbfe9e3156e71
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-01-31 16:26:38 +0200

    3

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 // let x, y;
 
 // block.onmousedown = e => {
+
+//     console.log(e);
 //     x = e.screenX;
 //     y = e.screenY;
 // };
@@ -21,46 +23,46 @@
 // };
 // !!!!!!!!!!!!!!!!!!!!!!!!
 
-// const block = document.querySelector(".block");
+const block = document.querySelector(".block");
 
-// let x, y;
-// let isBlockActive = false;
+let x, y;
+let isBlockActive = false;
 
-// block.onmousedown = e => {
-//     x = e.screenX;
-//     y = e.screenY;
+block.onmousedown = e => {
+    x = e.screenX;
+    y = e.screenY;
 
-//     isBlockActive = true;
-// };
+    isBlockActive = true;
+};
 
-// block.onmousemove = e => {
-//     if (isBlockActive) {
-//         let _x = e.screenX;
-//         let _y = e.screenY;
+block.onmousemove = e => {
+    if (isBlockActive) {
+        let _x = e.screenX;
+        let _y = e.screenY;
 
-//         diff_x = _x - x;
-//         diff_y = _y - y;
+        diff_x = _x - x;
+        diff_y = _y - y;
 
-//         block.style.left = !block.style.left ? `${diff_x}px` : `${parseInt(block.style.left) + diff_x}px`;
-//         block.style.top = !block.style.top ? `${diff_y}px` : `${parseInt(block.style.top) + diff_y}px`;
+        block.style.left = !block.style.left ? `${diff_x}px` : `${parseInt(block.style.left) + diff_x}px`;
+        block.style.top = !block.style.top ? `${diff_y}px` : `${parseInt(block.style.top) + diff_y}px`;
 
-//         x = _x;
-//         y = _y;
-//     }
-// }
+        x = _x;
+        y = _y;
+    }
+}
 
-// block.onmouseup = () => isBlockActive = false;
+block.onmouseup = () => isBlockActive = false;
 // !!!!!!!!!!!!!!!!!!!!
 
 // ! keypress, keydown, keyup
 
-const block = document.querySelector(".block");
+// const block = document.querySelector(".block");
 
 // document.addEventListener("keypress", e => console.log("keypress", e.keyCode));
 // document.addEventListener("keydown", e => console.log("keydown", e.keyCode));
 // document.addEventListener("keyup", e => console.log("keyup", e.keyCode));
 
-const STEP = 10;
+// const STEP = 10;
 
 // ! способ 1
 // document.addEventListener("keydown", e => {
@@ -72,17 +74,19 @@ const STEP = 10;
 
 // ! способ 2
 
-const operations = {
-    37: block => block.style.left = !block.style.left ? `${STEP}px` : `${parseInt(block.style.left) - STEP}px`,
-    38: block => block.style.top = !block.style.top ? `${STEP}px` : `${parseInt(block.style.top) - STEP}px`,
-    39: block => block.style.left = !block.style.left ? `${STEP}px` : `${parseInt(block.style.left) + STEP}px`,
-    40: block => block.style.top = !block.style.top ? `${STEP}px` : `${parseInt(block.style.top) + STEP}px`,
-}
+// const operations = {
+//     37: block => block.style.left = !block.style.left ? `${STEP}px` : `${parseInt(block.style.left) - STEP}px`,
+//     38: block => block.style.top = !block.style.top ? `${STEP}px` : `${parseInt(block.style.top) - STEP}px`,
+//     39: block => block.style.left = !block.style.left ? `${STEP}px` : `${parseInt(block.style.left) + STEP}px`,
+//     40: block => block.style.top = !block.style.top ? `${STEP}px` : `${parseInt(block.style.top) + STEP}px`,
+// }
 
-document.addEventListener("keydown", e => operations[e.keyCode](block));
+// document.addEventListener("keydown", e => operations[e.keyCode](block));
 
+// !!!!!!!!!!!!!!!!!!!!
 
-// form, e.preventDefault();
+// ! form, e.preventDefault()
+
 // const form = document.getElementById("form");
 // const button = document.getElementById("submit-form");
 // const firstName = document.getElementById("firstName");
@@ -97,40 +101,40 @@ document.addEventListener("keydown", e => operations[e.keyCode](block));
 // }
 
 // firstName.addEventListener("input", () => {
-//     if(firstName.value && firstName.value.length > 3) {
+//     if (firstName.value && firstName.value.length > 3) {
 //         array[0] = true
 //     } else {
 //         array[0] = false
 //         button.setAttribute("disabled", "disabled");
 //     }
 
-//     if(validation()) button.removeAttribute("disabled");
+//     if (validation()) button.removeAttribute("disabled");
 // })
 
 // lastName.addEventListener("input", () => {
-//     if(lastName.value && lastName.value.length > 3) {
+//     if (lastName.value && lastName.value.length > 3) {
 //         array[1] = true
 //     } else {
 //         array[1] = false
 //         button.setAttribute("disabled", "disabled");
 //     }
 
-//     if(validation()) button.removeAttribute("disabled");
+//     if (validation()) button.removeAttribute("disabled");
 // })
 
 // age.addEventListener("input", () => {
-//     if(age.value && age.value > 18) {
+//     if (age.value && age.value > 18) {
 //         array[2] = true
 //     } else {
 //         array[2] = false
 //         button.setAttribute("disabled", "disabled");
 //     }
 
-//     if(validation()) button.removeAttribute("disabled");
+//     if (validation()) button.removeAttribute("disabled");
 // })
 
 // form.addEventListener("click", e => {
-//     e.preventDefault(); 
+//     e.preventDefault();
 // })
 
 
